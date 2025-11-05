2f93f3213e52a77c21aecbc92cfb73e04ce55d35
roygicheru <gicheru.k.roy@gmail.com>
2025-11-05 15:14:13 +0300

    added for loop to count in task three

--- src/TaskThree/TaskThree.java
+++ src/TaskThree/TaskThree.java
@@ -18,6 +18,21 @@ public class TaskThree {
             System.out.print("How many numbers wil you enter? ");
 
             count = input.nextInt();
+
+            if (count > 0) {
+            for(int i = 0; i < count; i++){
+                try{
+                   System.out.print("Enter number " + (i + 1) + " of " + count + ": ");
+                   int number = input.nextInt();
+                   totalSum += number;
+                }
+                catch(InputMismatchException e){
+                    System.out.println("Invalid input. Please enter a valid whole number");
+                    input.next();
+                    i--;
+                }
+            }
+        }
             
         } 
         catch(InputMismatchException e){

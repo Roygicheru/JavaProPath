4d122e5129d82390bd2d4df6498abedc663c8f6b
roygicheru <gicheru.k.roy@gmail.com>
2025-11-10 14:08:22 +0300

    Added try catch and scanner to task four

--- src/TaskFour/TaskFour.java
+++ src/TaskFour/TaskFour.java
@@ -1,7 +1,24 @@
 package TaskFour;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class TaskFour {
     public void mainTaskFour(){
-        
+        Scanner input = null;
+
+        try {
+            input = new Scanner(System.in);
+            System.out.println("Starting challenge 7: While Loops and Averaging...");
+        } 
+        catch (InputMismatchException e) {
+            
+        }
+        finally {
+            if (input != null) {
+                System.out.println("Scanner resource successfully closed.");
+                input.close();
+            }
+        }
     }
 }

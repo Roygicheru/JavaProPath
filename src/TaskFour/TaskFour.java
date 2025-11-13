dcbdd76fc14982d08746c44331227e2fcd042d05
roygicheru <gicheru.k.roy@gmail.com>
2025-11-13 10:48:14 +0300

    Added loop logic and code resilience to taskFour.java

--- src/TaskFour/TaskFour.java
+++ src/TaskFour/TaskFour.java
@@ -16,10 +16,28 @@ public class TaskFour {
             "While Loops and Averaging...");
             System.out.println("Initial totalSum: " + totalSum);
             System.out.println("Initial count: " + count);
+
+            while (score != -1) {
+                try {
+                System.out.print("Enter a score "+
+                "(or -1 to quit): ");
+                score = input.nextInt();
+                if (score != -1 && score >= 1 && score <= 100) {
+                    totalSum += score;
+                    count++;
+                    System.out.println("Score entered: " + totalSum);
+                } else if (score != -1 && (score < 1 || score > 100)) {
+                    System.out.println("Invalid input. Please enter a "+
+                    "positive score only...upto 100, or -1 to quit.");
+                    continue;
+                }
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid input. Please enter a valid integer score or -1 to quit.");
+                    input.next(); // Clear the invalid input
+                    continue;
+                }
+            }
         } 
-        catch (InputMismatchException e) {
-            
-        }
         finally {
             if (input != null) {
                 System.out.println("Scanner resource"+

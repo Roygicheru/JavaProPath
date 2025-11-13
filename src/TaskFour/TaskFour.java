7fcb5b80ed07c93ee24d551ad09d9f12d3bd564c
roygicheru <gicheru.k.roy@gmail.com>
2025-11-13 09:01:04 +0300

    Updated variables and basic structure of task four.java

--- src/TaskFour/TaskFour.java
+++ src/TaskFour/TaskFour.java
@@ -6,17 +6,24 @@ import java.util.Scanner;
 public class TaskFour {
     public void mainTaskFour(){
         Scanner input = null;
+        double totalSum = 0.0;
+        int count = 0;
+        int score = 0;
 
         try {
             input = new Scanner(System.in);
-            System.out.println("Starting challenge 7: While Loops and Averaging...");
+            System.out.println("Starting challenge 7: "+
+            "While Loops and Averaging...");
+            System.out.println("Initial totalSum: " + totalSum);
+            System.out.println("Initial count: " + count);
         } 
         catch (InputMismatchException e) {
             
         }
         finally {
             if (input != null) {
-                System.out.println("Scanner resource successfully closed.");
+                System.out.println("Scanner resource"+
+                " successfully closed.");
                 input.close();
             }
         }

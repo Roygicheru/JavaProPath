e353e750a512d5a8cdc227f105776decdf98ee37
roygicheru <gicheru.k.roy@gmail.com>
2025-11-18 14:27:29 +0300

    Added a new method findLowestExpense

--- src/TaskFive/FinanceManager.java
+++ src/TaskFive/FinanceManager.java
@@ -3,11 +3,27 @@ package TaskFive;
 import java.util.ArrayList;
 
 public class FinanceManager {
+
     public static double findLowestExpense(ArrayList<Double> expenses) {
-        return 0.0;
+        if (expenses.isEmpty()) {
+            return Double.MAX_VALUE;
+        } else {
+            return 0.0;
+        }
+        double minExpense = expenses.get(0);
     }
+
     public void runFinanceManager(){
         System.out.println("Starting Finance Module 1: "+
         "Expense Utility...");
+
+        ArrayList<Double> monthlyExpenses = new ArrayList<>();
+        double[] expenseValues = {45.99,120.50,8.75,230.00,15.25};
+
+        for(int i = 0; i < expenseValues.length; i++){
+            monthlyExpenses.add(expenseValues[i]);
+        }
+
+        System.out.println("Monthly Expenses: " + monthlyExpenses);
     }
 }

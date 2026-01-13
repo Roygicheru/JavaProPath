a4ed84b23115677a1535a222fb9b3c8fb87375ec
roygicheru <gicheru.k.roy@gmail.com>
2026-01-13 10:44:45 +0300

    Added new folder session task

---
+++ internshipsessions/src/main/java/com/github/roygicheru/App.java
@@ -0,0 +1,15 @@
+package com.github.roygicheru;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // System.out.println( "Hello World!" );
+        com.github.roygicheru.SystechIntern.SessionTask1 sessionTask1 = new com.github.roygicheru.SystechIntern.SessionTask1();
+        sessionTask1.internshipTask();
+    }
+}
---
+++ internshipsessions/src/main/java/com/github/roygicheru/SystechIntern/SessionTask1.java
@@ -0,0 +1,7 @@
+package com.github.roygicheru.SystechIntern;
+
+public class SessionTask1 {
+    public void internshipTask(){
+        System.out.println("Hello World!");
+    }
+}
